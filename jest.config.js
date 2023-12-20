d7c48a6d87486398fd5baaf1b404e0e352cdc1e8
Ajay Gupta <itsajayg@icloud.com>
2023-12-20 12:14:17 +0530

    enable coverage

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   preset: "jest-expo",
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov", "clover", "html"],
   transform: {
     "^.+\\.[jt]sx?$": "babel-jest",
   },
