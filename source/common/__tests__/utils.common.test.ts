82d085d5d43dd3f6e3c30a3b9b341d4c936c29a2
Ajay Gupta <itsajayg@icloud.com>
2024-08-01 19:57:14 +0530

    setup ts

---
+++ source/common/__tests__/utils.common.test.ts
@@ -0,0 +1,11 @@
+// bun run test source/common/__tests__/utils.common.test.ts
+
+import { getUtcTimestamp } from "@app/common/utils.common";
+
+describe("utils.common", () => {
+  describe("getUtcTimestamp", () => {
+    it("should return a number", () => {
+      expect(typeof getUtcTimestamp()).toBe("number");
+    });
+  });
+});
